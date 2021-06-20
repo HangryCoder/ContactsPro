74c98902995e9c19fa121011e76fd9995bc57c93
HangrySonia <sonia.wadji@unacademy.com>
2021-06-20 15:13:00 +0530

    Added Epoxy and Fast Scroller dependencies

--- app/src/main/java/com/hangrycoder/contactspro/fragment/ContactsFragment.kt
+++ app/src/main/java/com/hangrycoder/contactspro/fragment/ContactsFragment.kt
@@ -5,7 +5,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.RecyclerView
 import com.hangrycoder.contactspro.R
+import kotlinx.android.synthetic.main.fragment_contacts.view.*
+
 
 class ContactsFragment : Fragment() {
 
@@ -16,6 +19,11 @@ class ContactsFragment : Fragment() {
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
         val view = inflater.inflate(R.layout.fragment_contacts, container, false)
+
+        val recyclerView = view.findViewById<RecyclerView>(R.id.contacts_recycler_view)
+
+        view.recycler_view_fast_scroller.setRecyclerView(recyclerView)
+
         return view
     }
 }
