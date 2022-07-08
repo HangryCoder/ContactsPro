2d1d7e9b77192c0ee24272801e9f8968bfde70e5
Sonia <soniawadji@gmail.com>
2022-07-08 12:10:54 +0530

    Merged with main and updated kotlin version. Forced to light mode temporarily

--- app/src/main/java/com/hangrycoder/contactspro/MainActivity.kt
+++ app/src/main/java/com/hangrycoder/contactspro/MainActivity.kt
@@ -2,6 +2,7 @@ package com.hangrycoder.contactspro
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -9,6 +10,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        //Temporary hack
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
 
         val contactsPagerAdapter = ContactsPagerAdapter(supportFragmentManager)
         viewPager.adapter = contactsPagerAdapter
